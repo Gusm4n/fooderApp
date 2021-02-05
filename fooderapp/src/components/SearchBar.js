141b35acfaa7c1f44d6678af4b64de034f1fca59
Felipe <felipe.unip.adm2015@gmail.com>
2021-02-05 19:03:12 -0300

    Barra de pesquisa

--- fooderapp/src/components/SearchBar.js
+++ fooderapp/src/components/SearchBar.js
@@ -1,9 +1,11 @@
 import './SearchBar.css';
+import { VscBell } from "react-icons/vsc";
 
 function SearchBar() {
   return (
     <div className="SearchBar">
-        <input type="text" className="SearchBox"></input>
+        <input type="text" className="SearchBox" placeholder="Exemplo: Rua Turiassu no 74"></input>
+        <div className="NotifiBell"><VscBell /></div>
     </div>
   );
 }
